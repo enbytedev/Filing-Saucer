7282a091cba6fe0f717aea47eb8cfbae5359d52c
enbyte <kieran@enbyte.dev>
2022-06-15 20:36:43 -0700

    Small fix-ups.

--- scripts/control.js
+++ scripts/control.js
@@ -23,15 +23,12 @@ Number generator
 function generate(n) {
   var add = 1,
   max = 12 - add;
-  
   if (n > max) {
     return generate(max) + generate(n - max);
   }
-  
   max = Math.pow(10, n + add);
   var min = max / 10; // Math.pow(10, n) basically 
   var number = Math.floor(Math.random() * (max - min + 1)) + min;
-  
   return ("" + number).substring(add);
 }
 
