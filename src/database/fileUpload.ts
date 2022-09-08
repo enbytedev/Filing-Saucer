b6fd53a3f8521a0a0f7bb0db1b488441691e172f
enbytedev <me@enbyte.dev>
2022-09-08 14:02:22 -0700

    Uploads are now written to the uploads table.

--- src/database/fileUpload.ts
+++ src/database/fileUpload.ts
@@ -2,5 +2,5 @@ import { connection } from './databaseDao.js';
 
 
 export default async (email: string, filename: string) => {
-    connection.execute('INSERT INTO `uploads` (`email`, `filename`, `date`) VALUES (?, ?, ?)', [email, filename, Date.now.toString()]);
+    connection.execute('INSERT INTO `uploads` (`email`, `filename`, `date`) VALUES (?, ?, ?)', [email, filename, Date.now().toString()]);
 }
--- src/express/routing/upload/upload.ts
+++ src/express/routing/upload/upload.ts
@@ -3,31 +3,32 @@ import path from 'path';
 import util from 'util';
 import { v1 as uuidv1 } from 'uuid';
 import crypto from 'crypto';
-// import databaseDao from '../../../database/databaseDao.js';
-
-const storageConfig = multer.diskStorage({   
-    destination: 'uploads', 
-      filename: (_req, file, cb) => {
-          cb(null, genName()
-             + path.extname(file.originalname))
-    }
-});
+import databaseDao from '../../../database/databaseDao.js';
+import { UserSessionInterface } from '../sessionInterfaces.js';
 
 const upload = util.promisify(multer({
-    storage: storageConfig,
+    storage: multer.diskStorage({   
+        destination: 'uploads', 
+          filename: (req, file, cb) => {
+              cb(null, genName((req.session as UserSessionInterface).email, path.extname(file.originalname)) + path.extname(file.originalname))
+        }
+    }),
     limits: {
       fileSize: 1000000 * 100 // 100 MB max file size
     },
 }).single('file'));
 
-function genName() {
-    let name = crypto.createHash('shake256', {outputLength: 8}).update(uuidv1()).update(crypto.randomBytes(256)).digest("hex");
+function genName(email: any, ext: string) {
+    let name = crypto.createHash('shake256', {outputLength: 8}).update(uuidv1()).update(email).update(crypto.randomBytes(256)).digest("hex");
+    name = name + ext;
+    writeUploadToDatabase(email, name);
     return name;
+}
+
+function writeUploadToDatabase(email: any, name: string) {
+    databaseDao.newFileUpload(email, name);
 }
 
 export default async (req: any, res: any) => {
     upload(req, res);
-    // TODO: Store and pass genName() to databaseDao.newFileUpload()
-    // databaseDao.newFileUpload(req.session.email, name);
-    
 }
