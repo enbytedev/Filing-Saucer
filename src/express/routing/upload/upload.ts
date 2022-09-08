05fc1450eae9d0f734f57bb1a21c2366087f573b
enbytedev <me@enbyte.dev>
2022-09-08 16:25:27 -0700

    Upload size is now enforced and properly handled.

--- src/express/routing/upload/upload.ts
+++ src/express/routing/upload/upload.ts
@@ -5,23 +5,22 @@ import { v1 as uuidv1 } from 'uuid';
 import crypto from 'crypto';
 import databaseDao from '../../../database/databaseDao.js';
 import { UserSessionInterface } from '../sessionInterfaces.js';
+import config from '../../../setup/config.js';
+import { renderDash } from '../user/dash.js';
 
 const upload = util.promisify(multer({
     storage: multer.diskStorage({   
-        destination: 'uploads', 
-          filename: (req, file, cb) => {
-              cb(null, genName((req.session as UserSessionInterface).email, path.extname(file.originalname)) + path.extname(file.originalname))
-        }
+        destination: 'uploads',
+        filename: (req, file, cb) => { cb(null, genName((req.session as UserSessionInterface).email, path.extname(file.originalname))); }
     }),
-    limits: {
-      fileSize: 1000000 * 100 // 100 MB max file size
-    },
+    limits: { fileSize: 1000000 * Number(config.maxFileSizeMB) },
 }).single('file'));
 
+let name = '';
+
 function genName(email: any, ext: string) {
-    let name = crypto.createHash('shake256', {outputLength: 8}).update(uuidv1()).update(email).update(crypto.randomBytes(256)).digest("hex");
+    name = crypto.createHash('shake256', {outputLength: 8}).update(uuidv1()).update(email).update(crypto.randomBytes(256)).digest("hex");
     name = name + ext;
-    writeUploadToDatabase(email, name);
     return name;
 }
 
@@ -30,5 +29,14 @@ function writeUploadToDatabase(email: any, name: string) {
 }
 
 export default async (req: any, res: any) => {
-    upload(req, res);
+        try {
+            await upload(req, res);
+            if (req.file == undefined) { renderDash(req, res, `no file selected...`); }
+            else {
+            writeUploadToDatabase((req.session as UserSessionInterface).email, name);
+            renderDash(req, res, `file uploaded successfully...`);
+            }
+        } catch (err: any) {
+            if (err.code == 'LIMIT_FILE_SIZE') { renderDash(req, res, `filesize exceeded ${config.maxFileSizeMB}MB; upload aborted...`); }
+        }
 }
--- src/express/routing/user/dash.ts
+++ src/express/routing/user/dash.ts
@@ -1,7 +1,12 @@
 import { Response, Request } from 'express';
 import { UserSessionInterface } from '../sessionInterfaces.js';
+import config from '../../../setup/config.js';
 
 export default async (req: Request, res: Response) => {
+    renderDash(req, res, `max file size is ${config.maxFileSizeMB}MB`);
+}
+
+export function renderDash(req: Request, res: Response, message: string) {
     let name = ((req.session as UserSessionInterface).firstName)?.toLowerCase();
-    res.render('user/dash.ejs', { name: name, panel: "<h2><i>what would you like to do</i></h2>" });
+    res.render('user/dash.ejs', { name: name, info: `<h3><i>${message}</i></h3>` });
 }
