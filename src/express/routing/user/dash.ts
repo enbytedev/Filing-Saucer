bb5944a57d635212eaeac87f6b0ed41208561f68
enbytedev <me@enbyte.dev>
2022-09-09 01:17:06 -0700

    A random greeting is now chosen when loading dash.

--- src/express/routing/user/dash.ts
+++ src/express/routing/user/dash.ts
@@ -8,5 +8,10 @@ export default async (req: Request, res: Response) => {
 
 export function renderDash(req: Request, res: Response, message: string) {
     let name = ((req.session as UserSessionInterface).firstName)?.toLowerCase();
-    res.render('user/dash.ejs', { name: name, info: `<h3><i>${message}</i></h3>` });
+    res.render('user/dash.ejs', { name: name, greeting: getRandomGreeting(), info: `<h3><i>${message}</i></h3>` });
+}
+
+export function getRandomGreeting() {
+    let greetings = ["hello", "hi there", "hola", "bonjour", "guten tag", "salve"];
+    return greetings[Math.floor(Math.random() * greetings.length)];
 }
