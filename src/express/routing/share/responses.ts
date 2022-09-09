6b1b57b897a08f7c1d641e95dc9722925f411753
enbytedev <me@enbyte.dev>
2022-09-09 01:31:20 -0700

    Fixed private setting on view and download routes

--- src/express/routing/share/responses.ts
+++ src/express/routing/share/responses.ts
@@ -1,17 +1,24 @@
 import { Response, Request } from 'express';
 import config from '../../../setup/config.js';
 import path from 'path';
+import databaseDao from '../../../database/databaseDao.js';
 
 const uploadDirectory = path.format({dir: config.uploadDirectory, base: ''});
 
-export function downloadFile(req: Request, res: Response) {
-    res.download(uploadDirectory + req.params.name, (err) => {
-        if (err) { res.render('basic/notFound.ejs'); }
+export async function downloadFile(req: Request, res: Response) {
+    databaseDao.getUserNameFromFile(req.params.name, async (userFirstName: any) => {
+        if (await databaseDao.isFilePrivate(req.params.name)) { res.render('share/private.ejs', {fileName: req.params.name, userFirstName: userFirstName}); return; }
+        res.download(uploadDirectory + req.params.name, (err) => {
+            if (err) { res.render('basic/notFound.ejs'); }
+        });
     });
 }
 
-export function viewFile(req: Request, res: Response) {
-    res.sendFile(req.params.name, {root: uploadDirectory}, (err) => {
-        if (err) { res.render('basic/notFound.ejs'); }
+export async function viewFile(req: Request, res: Response) {
+        databaseDao.getUserNameFromFile(req.params.name, async (userFirstName: any) => {
+        if (await databaseDao.isFilePrivate(req.params.name)) { res.render('share/private.ejs', {fileName: req.params.name, userFirstName: userFirstName}); return; }
+        res.sendFile(req.params.name, {root: uploadDirectory}, (err) => {
+            if (err) { res.render('basic/notFound.ejs'); }
+        });
     });
 }
