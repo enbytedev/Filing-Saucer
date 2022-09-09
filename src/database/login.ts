e6728512fb348079285cd6d2640483bc62f5eded
enbytedev <me@enbyte.dev>
2022-09-08 20:53:22 -0700

    Fixed crash when logging in with an email that doesn't exist

--- src/database/login.ts
+++ src/database/login.ts
@@ -4,9 +4,13 @@ import bcrypt from 'bcrypt';
 
 export default async (email: string, password: string, cb: Function) => {
     connection.execute('SELECT `password` FROM `users` WHERE `email` = ?', [email]).then((results: any) => {
-        bcrypt.compare(password, results[0][0].password, function(_err: any, isCorrect: boolean) {
-            cb(isCorrect);
-            return;
-        });
+        if (results[0].length === 0) {
+            cb(false);
+        } else {
+            bcrypt.compare(password, results[0][0].password, function(_err: any, isCorrect: boolean) {
+                cb(isCorrect);
+                return;
+            });
+        }
     });
 }
