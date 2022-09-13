c39c17c02cf1901f4e32ae728450a07f937bf1e9
enbytedev <me@enbyte.dev>
2022-09-13 14:06:29 -0700

    Timezones are now used on the history page.

--- src/express/routing/user/history.ts
+++ src/express/routing/user/history.ts
@@ -1,17 +1,18 @@
 import { Response, Request } from 'express';
 import { UserSessionInterface } from '../sessionInterfaces.js';
-import databaseDao from '../../../database/databaseAccess.js';
+import databaseAccess from '../../../database/databaseAccess.js';
 
 export default async (req: Request, res: Response) => {
     renderHistory(req, res, "");
 }
 
 export async function renderHistory(req: Request, res: Response, message: string) {
-    databaseDao.getInfo.getHistory((req.session as UserSessionInterface).email, async (history: any) => {
+    let email = String((req.session as UserSessionInterface).email);
+    databaseAccess.getInfo.getHistory(email, async (history: any) => {
         var count = history.length;
         let bundles: Array<any> = [];
         for (var i = 0; i < count; i++) {
-            bundles.push([history[i].filename, history[i].originalname, new Date(parseInt(history[i].date)).toLocaleString(), history[i].private[0] == 1]);
+            bundles.push([history[i].filename, history[i].originalname, new Date(parseInt(history[i].date)).toLocaleString('en-US', {timeZone: await databaseAccess.getInfo.getTimezoneFromEmail(email)}), history[i].private[0] == 1]);
         }
 
         res.render('user/history.ejs', { info: message, bundles: bundles });
